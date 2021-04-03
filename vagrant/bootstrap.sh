3adc7a97afcf25f510a79c2e3f837f761b898fd0
Anders Krarup <akr@rismasystems.com>
2021-04-03 20:44:18 +0200

    mo boiler plate

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -14,25 +14,17 @@ apt-get update
 # php specific version
 add-apt-repository -y ppa:ondrej/php
 
-#apt-get install -y apache2
 apt-get install -y php7.4 php7.4-psr php7.4-phalcon php7.4-cli php7.4-curl php7.4-gd php7.4-intl php7.4-mbstring php7.4-xml php7.4-xsl php7.4-zip php7.4-xdebug
-#apt-get install -y  php7.4-fpm
-#apt-get install -y php7.4-mysql
 apt-get install -y php7.4-mongodb
 apt-get install -y php-msgpack php-gettext php-redis
 
 apt-get install -y redis-server
 apt-get install -y mongodb-org
 
-#a2enmod actions alias proxy_fcgi
-
-#service php7.4-fpm restart
-#service apache2 restart
-
 #unlink /etc/apache2/sites-enabled/000-default.conf
 #ln -fs /var/www/andkrup.dk/vagrant/apache.conf /etc/apache2/sites-enabled/andkrup.dk.conf
 #ln -fs /var/www/andkrup.dk/vagrant/php7.4-fpm.ini /etc/php/7.4/fpm/php.ini
-ln -fs /var/www/andkrup.dk/vagrant/php7.4-cli.ini /etc/php/7.4/cli/php.ini
+ln -fs /opt/wsTools/vagrant/php7.4-cli.ini /etc/php/7.4/cli/php.ini
 
 # phpunit
 wget https://phar.phpunit.de/phpunit.phar -O /usr/local/bin/phpunit
