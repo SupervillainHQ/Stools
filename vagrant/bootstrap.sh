0c5630c01c1b720cccdbe6d397d85e03d443e045
Anders Krarup <andkrup@gmail.com>
2022-05-20 20:48:40 +0200

    update from win10

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,7 +7,8 @@ apt-get update
 apt-get upgrade -y
 
 # alternate nodejs install, fixing some problems with NodeJs v.4.2.6 on Ubuntu 16.04 (see https://stackoverflow.com/questions/46360567/error-npm-is-known-not-to-run-on-node-js-v4-2-6)
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+apt-get install -y nodejs
 
 # mongo (https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/)
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
@@ -20,22 +21,26 @@ add-apt-repository -y ppa:ondrej/php
 apt-get install -y php7.4 php7.4-psr php7.4-phalcon php7.4-cli php7.4-curl php7.4-gd php7.4-intl php7.4-mbstring php7.4-xml php7.4-xsl php7.4-zip php7.4-xdebug
 apt-get install -y php7.4-mongodb
 apt-get install -y php7.4-sqlite3
+
+update-alternatives --set php /usr/bin/php7.4
+
 apt-get install -y php-msgpack php-gettext php-redis
 
 apt-get install -y redis-server
 apt-get install -y mongodb-org
-apt-get install -y gettext nodejs zip
+apt-get install -y gettext zip
 
 systemctl enable mongod
 
 service mongod restart
 
-# Fix npm permissions by transferring global scope to vagrant user
-# !! This still requires a manual global install (as vagrant user) of npm-packages required by the project
+# Fix permission issues
+#  (https://stackoverflow.com/questions/48910876/error-eacces-permission-denied-access-usr-local-lib-node-modules)
 mkdir /home/vagrant/.npm-global
 chown vagrant:vagrant /home/vagrant/.npm-global
 npm config set prefix '/home/vagrant/.npm-global'
 echo "export PATH=/home/vagrant/.npm-global/bin:\$PATH" >> /home/vagrant/.profile
+chown -R vagrant:vagrant /usr/lib/node_modules
 
 #unlink /etc/apache2/sites-enabled/000-default.conf
 ln -fs /opt/wsTools/vagrant/php7.4-cli.ini /etc/php/7.4/cli/php.ini
